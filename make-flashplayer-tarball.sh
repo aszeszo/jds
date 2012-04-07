38b32338b9a2180e8d3846cd2ec1d29d79e10528
Andrzej Szeszo <aszeszo@gmail.com>
2012-04-07 15:17:43 +0100

    Add script to generate Flash player plugin tarball

---
+++ make-flashplayer-tarball.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VERS=11.1.102.55
+
+[ -f flash_player_11_solaris_x86.tar.bz2 ] || wget http://download.macromedia.com/pub/flashplayer/pdc/$VERS/flash_player_11_solaris_x86.tar.bz2
+[ -f flash_player_11_solaris_sparc.tar.bz2 ] || wget http://download.macromedia.com/pub/flashplayer/pdc/$VERS/flash_player_11_solaris_sparc.tar.bz2
+
+bzip2 -dc flash_player_11_solaris_x86.tar.bz2 | tar xf -
+bzip2 -dc flash_player_11_solaris_sparc.tar.bz2 | tar xf -
+
+mkdir flashplayer
+mv flash_player_solaris_*_x86 flashplayer/x86
+mv flash_player_solaris_*_sparc flashplayer/sparc
+
+tar cf - flashplayer | gzip -9 >flashplayer-$VERS-bin-solaris.tar.gz
+
+rm -rf flashplayer
