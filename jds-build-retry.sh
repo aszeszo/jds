82097008b135c5180bef9a7b911293f6706d017e
Andrzej Szeszo <aszeszo@gmail.com>
2012-05-30 00:36:46 +0100

    Update jds-build-retry.sh script to preserve logs of successful package builds

--- jds-build-retry.sh
+++ jds-build-retry.sh
@@ -3,7 +3,9 @@
 TOPDIR=`cd \`dirname $0\`; pwd`
 BLDDIR=$TOPDIR/build.`uname -p`
 
-for i in $BLDDIR/logs/*.log; do grep ^INFO:\ .*PASSED $i >/dev/null && rm $i; done
+[ -d $BLDDIR/logs/passed ] || mkdir 
+
+for i in $BLDDIR/logs/*.log; do grep ^INFO:\ .*PASSED $i >/dev/null && mv $i $BLDDIR/logs/passed; done
 for i in $BLDDIR/logs/*.log; do rm $i; touch $BLDDIR/queue/`basename $i|sed s/\.log$/\.spec/`; done
 
 ./jds-build.sh -i
